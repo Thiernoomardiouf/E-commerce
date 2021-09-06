71cb5e3d06dd90bfa9f286f580eba21d7c73ea82
Thierno Omar Diouf <43839688+Thiernoomardiouf@users.noreply.github.com>
2021-09-06 14:15:30 +0000

    Add files via upload

---
+++ E-commerce-2/script.js
@@ -0,0 +1,10 @@
+var img = document.querySelectorAll("img");
+
+window.addEventListener("click",function cliquer(){
+      var i = Number(target.value);
+    // for (let i = 0; i < img.length; i++){
+      img[i].style.height = "700px";
+      img[i].style.width = "700px";
+    // }
+  });
+ 
